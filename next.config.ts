8d8e0fc9aa1cc680060ac29f0aed786631fb54ce
Thanawat Lovitayaolan <emotion.tuii@gmail.com>
2026-06-26 00:49:48 +0700

    Add redirects to upskilleveryday.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "https://www.upskilleveryday.com/tools/disc",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
